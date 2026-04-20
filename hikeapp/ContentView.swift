d554542a62210b6bf38f92ed3bdd960579430884
scooobeyrooobey <deske247@gmail.com>
2026-04-20 13:36:03 +0400

    Initial Commit

---
+++ hikeapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hikeapp
+//
+//  Created by scoobie roobie on 20/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hikeapp/hikeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeappApp.swift
+//  hikeapp
+//
+//  Created by scoobie roobie on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct hikeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
